3cbd6620241af2395c3df6eb9d4be939d6d6b612
Kevin_Isaza <2089276@talnet.nl>
2020-01-09 12:19:39 +0100

    Uitwerking van de exercise

---
+++ exercise.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `planeten` ADD `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY;
+INSERT INTO sterrenstelsel.planeten (naam, diameter, `afstand_tot_de_zon`, `massa`, bezoek_datum, id) VALUES ('Mars', 6794, 227936640, 0, null, null);
